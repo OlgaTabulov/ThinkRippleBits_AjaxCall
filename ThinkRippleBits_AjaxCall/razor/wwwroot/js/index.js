61dce5895977862cac5b4dbe97ce8a3696b6ec58
Olga Tabulov <42352901+OlgaTabulov@users.noreply.github.com>
2019-10-17 16:04:16 -0400

    Original
    Fist Commit

---
+++ ThinkRippleBits_AjaxCall/razor/wwwroot/js/index.js
@@ -0,0 +1,14 @@
+﻿function DoClick() {
+    var params = {};
+    var uri = 'api/random/GetSample/';
+    CallAjax(uri, params, DoClick_OnSuccess);
+}
+
+function DoClick_OnSuccess(data, status) {
+    if (status === 'success') {
+        $("#result").html(data);
+    }
+    else {
+        alert("error: " + data);
+    }
+}
---
+++ ThinkRippleBits_AjaxCall/razor/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿
+function CallAjax(url, params, success, error, type) {
+    url = (typeof url === 'string') ? url : "";
+    params = params || {};
+    success = success || defaultSuccessFunction;
+    error = error || defaultErrorFunction;
+    type = type || 'GET';
+
+    var respObj = $.ajax({
+        type: type,
+        url: url,
+        data: params,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: success,
+        error: error
+    });
+    return respObj;
+}
+
+var defaultSuccessFunction = function (xml, status, xhr) { };
+var defaultErrorFunction = function (XMLHttpRequest, textStatus, errorThrown) {
+    console.log("Status: " + textStatus);
+    console.log("Error: " + errorThrown);
+};
